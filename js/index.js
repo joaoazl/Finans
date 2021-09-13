dc63915367c138f4207813a0ed228ba6f6bb0b43
João Marcos <joaodias0078@gmail.com>
2021-09-13 09:58:42 -0400

    commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    $('.modal-container').css({display: 'none'}).appendTo(('.modal-container')).fadeIn(1000);
-    $('.modal-animate').css({display: 'none'}).appendTo(('.modal-animate')).show(1000);
+    $('.modal-container').fadeIn(1000);
+    $('.modal-animate').animate({opacity: 1, delay: 800, marginTop: '0px'}, 1200);
 
     $('#btn').click(function(){
         $('.modal-container').fadeOut(1000)
